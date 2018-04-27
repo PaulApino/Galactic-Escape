190cfd3749bbffee67bf3d8b0ad75d240b4e8343
Paul Apino <apinopaul@gmail.com>
2018-04-27 22:44:12 +1200

    Play around View for displaying and transformation of images.

--- app/src/main/java/com/gscape/sdp/galacticescape/Display/ActualGame/GamePlayDisplay.java
+++ app/src/main/java/com/gscape/sdp/galacticescape/Display/ActualGame/GamePlayDisplay.java
@@ -1,17 +1,36 @@
-package com.gscape.sdp.galacticescape.Display;
+package com.gscape.sdp.galacticescape.Display.ActualGame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.DragEvent;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.gscape.sdp.galacticescape.R;
 
 public class GamePlayDisplay extends AppCompatActivity {
 
+    private ImageView v1, v2;
+    private View contentView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_play_display);
+
+        contentView = findViewById(R.id.SomeLayout);
+        contentView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                v1.setX(motionEvent.getX());
+                v1.setY(motionEvent.getY());
+                return false;
+            }
+        });
+
+        v1 = findViewById(R.id.imageView5);
+        v2 = findViewById(R.id.imageView6);
     }
 
     @Override
