7738a6a27071bfed54bd1d856b9888bc9f3b055e
Paul Apino <apinopaul@gmail.com>
2018-04-28 17:38:31 +1200

    Figured out inCode View transformation.

--- app/src/main/java/com/gscape/sdp/galacticescape/Display/ActualGame/GamePlayDisplay.java
+++ app/src/main/java/com/gscape/sdp/galacticescape/Display/ActualGame/GamePlayDisplay.java
@@ -1,36 +1,72 @@
 package com.gscape.sdp.galacticescape.Display.ActualGame;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.DragEvent;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.ImageView;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.gscape.sdp.galacticescape.R;
 
 public class GamePlayDisplay extends AppCompatActivity {
 
-    private ImageView v1, v2;
-    private View contentView;
+    private TextView txt;
+    private ViewGroup contentView;
+    private int _xDelta, _yDelta;
+    private int contentHeight;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_play_display);
 
         contentView = findViewById(R.id.SomeLayout);
-        contentView.setOnTouchListener(new View.OnTouchListener() {
+        contentHeight = 0;
+        contentView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                v1.setX(motionEvent.getX());
-                v1.setY(motionEvent.getY());
-                return false;
+            public void onGlobalLayout() {
+                if (contentHeight == 0) {
+                    contentHeight = contentView.getHeight();
+                }
             }
         });
 
-        v1 = findViewById(R.id.imageView5);
-        v2 = findViewById(R.id.imageView6);
+        txt = findViewById(R.id.fullscreen_content);
+
+        txt.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                final int X = (int) motionEvent.getRawX();
+                final int Y = (int) motionEvent.getRawY();
+                switch (motionEvent.getAction() & MotionEvent.ACTION_MASK) {
+                    case MotionEvent.ACTION_DOWN:
+                        RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
+                        _xDelta = X - lParams.leftMargin;
+                        _yDelta = contentHeight - lParams.bottomMargin - Y;
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        break;
+                    case MotionEvent.ACTION_POINTER_DOWN:
+                        break;
+                    case MotionEvent.ACTION_POINTER_UP:
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
+                        layoutParams.leftMargin = X - _xDelta;
+                        layoutParams.bottomMargin = contentHeight - Y - _yDelta;
+                        view.setLayoutParams(layoutParams);
+                        break;
+                }
+                contentView.invalidate();
+                return true;
+            }
+        });
     }
 
     @Override
@@ -43,4 +79,6 @@ public class GamePlayDisplay extends AppCompatActivity {
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
     }
+
+
 }
