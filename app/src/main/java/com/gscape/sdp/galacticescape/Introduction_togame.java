cfb48fad8fae0d1dafd3e84f7aa0c2278a7f24bb
Aseni <sylvanzite@gmail.com>
2018-04-30 19:29:47 +1200

    Intro to game class and user name selection added

---
+++ app/src/main/java/com/gscape/sdp/galacticescape/Introduction_togame.java
@@ -0,0 +1,32 @@
+package com.gscape.sdp.galacticescape;
+
+import android.content.Intent;
+import android.sax.StartElementListener;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Introduction_togame extends AppCompatActivity {
+
+    Button skip_intro;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_introduction_togame);
+        skip_intro = findViewById(R.id.skip_intro_bt);
+
+        skip_intro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent skipped = new Intent(Introduction_togame.this, User_name_selection.class );
+                startActivity(skipped);
+            }
+        });
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/gscape/sdp/galacticescape/User_name_selection.java
@@ -0,0 +1,20 @@
+package com.gscape.sdp.galacticescape;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class User_name_selection extends AppCompatActivity {
+
+    EditText username;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_name_selection);
+
+
+    }
+}
