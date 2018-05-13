cd12cd071588bc3ebf2d16e53084e0a7a877a7dd
Aseni1 <sylvanzite@gmail.com>
2018-05-13 18:15:15 +1200

    FireBase Cloud Database created. Create new user feature added, and selecting current user added.

--- app/src/main/java/com/gscape/sdp/galacticescape/Introduction_togame.java
+++ app/src/main/java/com/gscape/sdp/galacticescape/Introduction_togame.java
@@ -30,7 +30,7 @@ public class Introduction_togame extends AppCompatActivity {
         skip_intro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent skipped = new Intent(Introduction_togame.this, GamePlayScreen.class );
+                Intent skipped = new Intent(Introduction_togame.this, User_name_selection.class );
                 startActivity(skipped);
             }
         });
