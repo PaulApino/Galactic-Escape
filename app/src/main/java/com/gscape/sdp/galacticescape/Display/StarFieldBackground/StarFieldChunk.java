d8d4c5cd667cb7268ca1d442e4a0afbe6b4e86c5
Paul Apino <apinopaul@gmail.com>
2018-05-15 08:50:02 +1200

    Start of static background starfield.

---
+++ app/src/main/java/com/gscape/sdp/galacticescape/Display/StarFieldBackground/StarFieldChunk.java
@@ -0,0 +1,20 @@
+package com.gscape.sdp.galacticescape.Display.StarFieldBackground;
+
+import com.gscape.sdp.galacticescape.Engine.Physics.Vector;
+
+import java.util.ArrayList;
+
+public class StarFieldChunk {
+
+    private final ArrayList<BackgroundStar> stars;
+    private final Vector chunkLocation;
+
+    public StarFieldChunk(Vector chunkLocation) {
+        this.chunkLocation = chunkLocation;
+        stars = new ArrayList<>(50);
+    }
+
+    public void generateChunk(int starCount) {
+
+    }
+}
---
+++ app/src/main/java/com/gscape/sdp/galacticescape/Display/StarFieldBackground/StarForge.java
@@ -0,0 +1,4 @@
+package com.gscape.sdp.galacticescape.Display.StarFieldBackground;
+
+public class StarForge {
+}
