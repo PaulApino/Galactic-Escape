26548228022d87fae1da8345d02c512aa9a5da21
Paul Apino <apinopaul@gmail.com>
2018-05-16 14:23:28 +1200

    Optimizes background star generation.

--- app/src/main/java/com/gscape/sdp/galacticescape/Display/StarFieldBackground/StarForge.java
+++ app/src/main/java/com/gscape/sdp/galacticescape/Display/StarFieldBackground/StarForge.java
@@ -5,8 +5,6 @@ import com.gscape.sdp.galacticescape.Engine.Physics.Vector;
 public class StarForge {
 
     private final String starSeed;
-    private String starStringSequence;
-    private int s_start, s_end, s_current;
 
     public StarForge (int seed) {
         int tempSeed = Math.abs(seed);
@@ -22,51 +20,59 @@ public class StarForge {
         } else {
             starSeed = s_tempSeed.substring(0,4);
         }
-        starStringSequence = "";
     }
 
     public void generateChunk (StarFieldChunk chunk, int starCount) {
-        initializeChunk(chunk.getChunkLocation());
+        String starStringSequence = "";
+
+        int arrStartNum[] = initializeChunk(chunk.getChunkLocation());
+        int s_start = arrStartNum[0];
+        int s_end = arrStartNum[1];
+        int s_current;
 
         int starStringsCounter = (starCount * 9) - 1;
         int starValuesCounter = 0;
 
-        for (int i = 1; starStringSequence.length() <= starStringsCounter; i++) {
-            s_current = getNextNum();
-            fixCurrent();
+        while (starStringSequence.length() < starStringsCounter) {
+            s_current = getNextNum(s_start, s_end);
+            s_current = fixCurrent(starStringSequence, s_current);
             starStringSequence += s_current;
             starValuesCounter++;
 
             if (starValuesCounter >= 3 && starValuesCounter % 3 == 0) {
-                chunk.addStar(getStar(starStringSequence.length()));
+                chunk.addStar(getStar(starStringSequence.length(), starStringSequence));
             }
             s_start = s_end;
             s_end = s_current;
         }
     }
 
-    private void initializeChunk (Vector chunkLocation) {
+    private int[] initializeChunk (Vector chunkLocation) {
         String chunkX = Integer.toString(Math.abs((int)chunkLocation.getX()) + 1000).substring(0,2);
         String chunkY = Integer.toString(Math.abs((int)chunkLocation.getY()) + 1000).substring(2, 4);
-        s_start = Integer.parseInt(starSeed.substring(0, 2)) + Integer.parseInt(chunkX);
-        s_end = Integer.parseInt(starSeed.substring(2, 4)) + Integer.parseInt(chunkY);
+        int startNum = Integer.parseInt(starSeed.substring(0, 2)) + Integer.parseInt(chunkX);
+        int endNum = Integer.parseInt(starSeed.substring(2, 4)) + Integer.parseInt(chunkY);
+        int[] arrInt = {startNum, endNum};
+        return arrInt;
     }
 
-    private int getNextNum () {
+    private int getNextNum (int s_start, int s_end) {
         return s_start + s_end;
     }
 
-    private void fixCurrent() {
+    private int fixCurrent(String starStringSequence, int s_current) {
         if (s_current > 999) {
-            s_current = Integer.parseInt(Integer.toString(s_current).substring(1, 4));
-        } else if (s_current < 10) {
-            starStringSequence += 00;
-        } else if (s_current < 100) {
-            starStringSequence += 0;
+            return Integer.parseInt(Integer.toString(s_current).substring(1, 4));
         }
+        if (s_current < 10) {
+            starStringSequence.concat("00");
+        } else if (s_current < 100) {
+            starStringSequence.concat("0");
+        }
+        return s_current;
     }
 
-    private BackgroundStar getStar (int index) {
+    private BackgroundStar getStar (int index, String starStringSequence) {
         double radius = Double.parseDouble(starStringSequence.substring(index - 1, index)) + 3;
         return new BackgroundStar(Vector.make2D(
                 Double.parseDouble(starStringSequence.substring(index - 9, index - 6)),
--- app/src/main/java/com/gscape/sdp/galacticescape/Display/TestDisplay/StarFieldTest.java
+++ app/src/main/java/com/gscape/sdp/galacticescape/Display/TestDisplay/StarFieldTest.java
@@ -1,19 +1,15 @@
 package com.gscape.sdp.galacticescape.Display.TestDisplay;
 
 import android.support.constraint.ConstraintLayout;
-import android.support.constraint.ConstraintSet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-import com.gscape.sdp.galacticescape.Display.StarFieldBackground.BackgroundStar;
 import com.gscape.sdp.galacticescape.Display.StarFieldBackground.StarFieldChunk;
 import com.gscape.sdp.galacticescape.Display.StarFieldBackground.StarFieldChunkView;
 import com.gscape.sdp.galacticescape.Display.StarFieldBackground.StarForge;
 import com.gscape.sdp.galacticescape.Engine.Physics.Vector;
 import com.gscape.sdp.galacticescape.R;
-
-import java.util.ArrayList;
 
 public class StarFieldTest extends AppCompatActivity {
 
@@ -22,7 +18,6 @@ public class StarFieldTest extends AppCompatActivity {
     private StarForge starForge;
     private StarFieldChunk chunk;
     private StarFieldChunkView chunkView;
-    private ArrayList<BackgroundStar> stars;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +26,90 @@ public class StarFieldTest extends AppCompatActivity {
 
         contentLayout = findViewById(R.id.test_starfield_layout);
 
-        starForge = new StarForge(5913);
+        starForge = new StarForge(8239);
         chunk = new StarFieldChunk(Vector.make2D(0,0));
-        starForge.generateChunk(chunk, 50);
+        starForge.generateChunk(chunk, 200);
+
         chunkView = chunk.getChunkView(getApplicationContext());
 
         contentLayout.addView(chunkView);
+
+        StarFieldChunk chunk1 = new StarFieldChunk(Vector.make2D(-1000,1000));
+        StarFieldChunk chunk2 = new StarFieldChunk(Vector.make2D(0,1000));
+        StarFieldChunk chunk3 = new StarFieldChunk(Vector.make2D(1000,1000));
+        StarFieldChunk chunk4 = new StarFieldChunk(Vector.make2D(-1000,0));
+        StarFieldChunk chunk5 = new StarFieldChunk(Vector.make2D(0,0));
+        StarFieldChunk chunk6 = new StarFieldChunk(Vector.make2D(1000,0));
+        StarFieldChunk chunk7 = new StarFieldChunk(Vector.make2D(-1000,-1000));
+        StarFieldChunk chunk8 = new StarFieldChunk(Vector.make2D(0,-1000));
+        StarFieldChunk chunk9 = new StarFieldChunk(Vector.make2D(1000,-1000));
+
+//        long start = System.nanoTime();
+//        Thread t1 = new Thread(new GeneratorRunnable(starForge, chunk1, 200));
+//        Thread t2 = new Thread(new GeneratorRunnable(starForge, chunk2, 200));
+//        Thread t3 = new Thread(new GeneratorRunnable(starForge, chunk3, 200));
+//        Thread t4 = new Thread(new GeneratorRunnable(starForge, chunk4, 200));
+//        Thread t5 = new Thread(new GeneratorRunnable(starForge, chunk5, 200));
+//        Thread t6 = new Thread(new GeneratorRunnable(starForge, chunk6, 200));
+//        Thread t7 = new Thread(new GeneratorRunnable(starForge, chunk7, 200));
+//        Thread t8 = new Thread(new GeneratorRunnable(starForge, chunk8, 200));
+//        Thread t9 = new Thread(new GeneratorRunnable(starForge, chunk9, 200));
+//        t1.start();
+//        t2.start();
+//        t3.start();
+//        t4.start();
+//        t5.start();
+//        t6.start();
+//        t7.start();
+//        t8.start();
+//        t9.start();
+//        try {
+//            t1.join();
+//            t2.join();
+//            t3.join();
+//            t4.join();
+//            t5.join();
+//            t6.join();
+//            t7.join();
+//            t8.join();
+//            t9.join();
+//        } catch (InterruptedException e) {}
+//        long end = System.nanoTime();
+//        end -= start;
+//
+//        Log.i("Generation Time", end + "");
+
+        long start = System.nanoTime();
+        starForge.generateChunk(chunk1, 200);
+        starForge.generateChunk(chunk2, 200);
+        starForge.generateChunk(chunk3, 200);
+        starForge.generateChunk(chunk4, 200);
+        starForge.generateChunk(chunk5, 200);
+        starForge.generateChunk(chunk6, 200);
+        starForge.generateChunk(chunk7, 200);
+        starForge.generateChunk(chunk8, 200);
+        starForge.generateChunk(chunk9, 200);
+        long end = System.nanoTime();
+        end -= start;
+
+        Log.i("Generation Time", end + "");
+    }
+
+    private class GeneratorRunnable implements Runnable {
+
+        private final StarForge starForge;
+        private final StarFieldChunk chunk;
+        private final int starCount;
+
+        public GeneratorRunnable(StarForge starForge, StarFieldChunk chunk, int starCount) {
+            this.starForge = starForge;
+            this.chunk = chunk;
+            this.starCount = starCount;
+        }
+
+        @Override
+        public void run() {
+            starForge.generateChunk(chunk, starCount);
+        }
     }
 }
